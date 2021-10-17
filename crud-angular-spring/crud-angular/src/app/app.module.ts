8ab2931a731d0879e99b3ef3319ed1b8c39f7172
Carvalho <felipee_sc@hotmail.com>
2021-10-17 12:46:50 -0300

    implement component login -app module

--- crud-angular-spring/crud-angular/src/app/app.module.ts
+++ crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
